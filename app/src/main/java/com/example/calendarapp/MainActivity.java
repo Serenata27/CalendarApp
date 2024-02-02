a16f201467c58b5e2e01ad7b99c0acecc86caaa1
chungmankim <chad8067@gmail.com>
2024-02-01 22:21:56 -0500

    New Main botton nav view

--- app/src/main/java/com/example/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -1,25 +1,63 @@
 package com.example.calendarapp;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.calendarapp.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = findViewById(R.id.add_exam);
-        fab.setOnClickListener(view -> openExamAdder());
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //FloatingActionButton fab = findViewById(R.id.add_exam);
+        //fab.setOnClickListener(view -> openExamAdder());
+
+        binding.bottomNavigation.setBackground(null);
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+           if( item.getItemId() == R.id.navhome ){
+               replaceFragment(new HomeFragment());
+           } else if (item.getItemId() == R.id.navclass) {
+               replaceFragment(new ClassFragment());
+           } else if (item.getItemId() == R.id.navassignment) {
+               replaceFragment(new AssignmentFragment());
+           } else if (item.getItemId() == R.id.navexam) {
+               replaceFragment(new ExamFragment());
+           } else if (item.getItemId() == R.id.navtodo) {
+               replaceFragment(new TodoFragment());
+           }
+           return true;
+        });
     }
 
     private void openExamAdder() {
         Intent intent = new Intent(this, ExamAdder.class);
         startActivity(intent);
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+
 }
