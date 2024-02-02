506ebfb02d7b853dd15f4e2c6d16d591da5c0302
chungmankim <chad8067@gmail.com>
2024-02-01 23:40:32 -0500

    New Main botton nav view

--- app/src/main/java/com/example/calendarapp/ClassListFragment.java
+++ app/src/main/java/com/example/calendarapp/ClassListFragment.java
@@ -2,29 +2,20 @@ package com.example.calendarapp;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-public class ClassFragment extends Fragment {
 
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class ClassListFragment extends Fragment {
 
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_class, container, false);
+        return inflater.inflate(R.layout.fragment_class_list, container, false);
     }
 }
--- app/src/main/java/com/example/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -10,24 +10,38 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 
 import com.example.calendarapp.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    EditText eiditCourseName,eiditCourseTime,eiditCourseInstructor;
+    Button btnClassAdd;
+    ListView listClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         //FloatingActionButton fab = findViewById(R.id.add_exam);
         //fab.setOnClickListener(view -> openExamAdder());
 
+
+        // this is for bottom navigation view
         binding.bottomNavigation.setBackground(null);
         binding.bottomNavigation.setOnItemSelectedListener(item -> {
 
@@ -44,6 +58,28 @@ public class MainActivity extends AppCompatActivity {
            }
            return true;
         });
+
+        //this is for fragment class
+        eiditCourseName = (EditText) findViewById(R.id.eiditCourseName);
+        eiditCourseTime = (EditText) findViewById(R.id.eiditCourseTime);
+        eiditCourseInstructor = (EditText) findViewById(R.id.eiditCourseInstructor);
+        btnClassAdd = (Button) findViewById(R.id.btnClassAdd);
+        listClass = (ListView) findViewById(R.id.listClass);
+        //ArrayList<String> classes = new ArrayList<>();
+        //ArrayAdapter<String> adapterClasses = new ArrayAdapter<String>(this,R.layout.fragment_class,R.id.listClass,classes);
+        listClass.setAdapter(adapterClasses);
+        btnClassAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String courseName = eiditCourseName.getText().toString();
+                String courseTime = eiditCourseTime.getText().toString();
+                String courseInstructor = eiditCourseInstructor.getText().toString();
+                classes.add("Course: "+courseName+" Time: "+courseTime+" Instructor: "+courseInstructor);
+            }
+
+        });
+
+
     }
 
     private void openExamAdder() {
