5700b17211f5a9214386726efd363efaed1ab344
chungmankim <chad8067@gmail.com>
2024-02-03 01:28:01 -0500

    Done Story 1,2,3

--- app/src/main/java/com/example/calendarapp/ExamFragment.java
+++ app/src/main/java/com/example/calendarapp/ExamFragment.java
@@ -1,90 +1,191 @@
 package com.example.calendarapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-public class ExamFragment extends Fragment {
+public class ExamFragment extends Fragment{
 
-    private ArrayList<Exam> exams;
-    private ArrayAdapter<Exam> adapter;
+    private ArrayList<Classes> classes;
+    private ArrayAdapter<Classes> adapter;
 
-    private EditText dateEditText, timeEditText, locationEditText;
-    private Button addButton;
-    private ListView examListView;
+    private EditText eiditCourseName, eiditCourseTime, eiditCourseInstructor;
+    private Button btnClassAdd,btnClassEdit;
+    private ListView listClass;
+    private String item;
+    private int indexVal;
+    private Classes classval;
 
     public ExamFragment() {
-        // Required empty public constructor
+        //Constructor
     }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadData();
 
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_exam, container, false);
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, classes);
 
-        exams = new ArrayList<>();
-        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, exams);
 
-        dateEditText = view.findViewById(R.id.editTextDate);
-        timeEditText = view.findViewById(R.id.editTextTime);
-        locationEditText = view.findViewById(R.id.editTextLocation);
-        addButton = view.findViewById(R.id.addButton);
-        examListView = view.findViewById(R.id.listViewExams);
-
-        examListView.setAdapter(adapter);
-
-        addButton.setOnClickListener(new View.OnClickListener() {
+        eiditCourseName = view.findViewById(R.id.editTextDate);
+        eiditCourseTime = view.findViewById(R.id.editTextTime);
+        eiditCourseInstructor = view.findViewById(R.id.editTextLocation);
+        btnClassAdd = view.findViewById(R.id.addButton);
+        btnClassEdit = view.findViewById(R.id.editExammBotton);
+        listClass = view.findViewById(R.id.listViewExams);
+        listClass.setAdapter(adapter);
+        //add data
+        btnClassAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addExam();
+                addClass();
             }
         });
+
+        //Update data
+        btnClassEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateClass();;
+            }
+        });
+
+        //getting index when click on listview
+        listClass.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                item = parent.getItemAtPosition(position).toString()+"has been selected";
+                indexVal = position;
+                Toast.makeText(getActivity(), item, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Delete listview class when double click
+        listClass.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                item = parent.getItemAtPosition(position).toString()+"has been deleted";
+                Toast.makeText(getActivity(), item, Toast.LENGTH_SHORT).show();
+                indexVal = position;
+                delete();
+                adapter.notifyDataSetChanged();
+                indexVal =0; //reset
+                return true;
+            }
+        });
+
+
 
         return view;
     }
 
-    private void addExam() {
-        String date = dateEditText.getText().toString();
-        String time = timeEditText.getText().toString();
-        String location = locationEditText.getText().toString();
+    private void addClass() {
+        String classes = eiditCourseName.getText().toString();
+        String time = eiditCourseTime.getText().toString();
+        String instructor = eiditCourseInstructor.getText().toString();
 
-        if (!date.isEmpty() && !time.isEmpty() && !location.isEmpty()) {
-            Exam newExam = new Exam(date, time, location);
-            exams.add(newExam);
+
+
+        if (!classes.isEmpty() && !time.isEmpty() && !instructor.isEmpty()) {
+            Classes newClass = new Classes(classes,time,instructor);
+            this.classes.add(newClass);
             adapter.notifyDataSetChanged();
 
             // Clear input fields
-            dateEditText.getText().clear();
-            timeEditText.getText().clear();
-            locationEditText.getText().clear();
+            eiditCourseName.getText().clear();
+            eiditCourseTime.getText().clear();
+            eiditCourseInstructor.getText().clear();
+        }
+        saveDate();
+    }
+
+    //Update class
+    private void updateClass(){
+        String classes = eiditCourseName.getText().toString();
+        String time = eiditCourseTime.getText().toString();
+        String instructor = eiditCourseInstructor.getText().toString();
+        Classes newClass = new Classes(classes,time,instructor);
+
+        this.classes.set(indexVal,newClass);
+        adapter.notifyDataSetChanged();
+
+        eiditCourseName.getText().clear();
+        eiditCourseTime.getText().clear();
+        eiditCourseInstructor.getText().clear();
+        indexVal = 0; //reset index
+        saveDate();
+
+    }
+
+    //Delete
+    private void delete(){
+        this.classes.remove(indexVal);
+        saveDate();
+    }
+    //save data
+    private void saveDate(){
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("classShare2", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(classes);
+        editor.putString("list2",json);
+        editor.apply();
+    }
+    private void loadData(){
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("classShare2", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("list2",null);
+        Type type = new TypeToken<ArrayList<Classes>>() {}.getType();
+        classes = gson.fromJson(json, type);
+
+        if(classes == null){
+            classes = new ArrayList<>();
         }
     }
 
-    private static class Exam {
-        private String date;
+    private static class Classes{
+        private String classes;
         private String time;
-        private String location;
+        private String instructor;
 
-        Exam(String date, String time, String location) {
-            this.date = date;
+        Classes(String classes, String time, String instructor) {
+            this.classes = classes;
             this.time = time;
-            this.location = location;
+            this.instructor = instructor;
         }
+        Classes(){}
 
         @NonNull
         @Override
         public String toString() {
-            return "Date: " + date + ", Time: " + time + ", Location: " + location;
+            return "Exam Date: " + classes + ", Time: " + time + ", Location: " + instructor;
         }
+
     }
+
 }
